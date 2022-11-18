2cd49debb717d3f54e3e1174536f150663afb267
Jaydippatel <macbook@MacBooks-MacBook-Pro.local>
2022-11-18 18:42:14 +0530

    add parketplace

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -1,8 +1,21 @@
+import { Box } from '@mui/material'
 import React from 'react'
 
 function Marketplace() {
   return (
-    <div>Marketplace</div>
+    <div className='container'>
+      <Box sx={{ width: '100%', padding: '0', marginTop: { xs: '50px', sm: '60px', md: '70px', lg: '100px' } }}>
+        <div className='footer-position '>
+          <div className='container mt-4' style={{ margin: '5% 0' }}>
+            <div className='row'>
+              <div className='col'>
+                <h4>Coming Soon...!</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Box>
+    </div>
   )
 }
 
